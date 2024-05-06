4d849ff14e596dec1fcc2e8ca8435954c7a77efc
José Rodrigues <joser0drigueswork@gmail.com>
2024-05-06 15:18:56 +0100

    Login com o RMI feito

--- src/main/java/springboot/AccountController.java
+++ src/main/java/springboot/AccountController.java
@@ -1,5 +1,8 @@
 package springboot;
 
+import common.ConnectionsEnum;
+import common.MetodosRMIGateway;
+import common.URLData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import java.rmi.Naming;
+import java.util.List;
 import java.util.Objects;
 
 @Controller
@@ -24,13 +29,32 @@ public class AccountController {
     // Login Action
     @PostMapping("/loginForm")
     public String loginForm(@RequestParam("username") String username,
-                        @RequestParam("password") String password, HttpServletResponse response) {
+                        @RequestParam("password") String password, HttpServletResponse response, Model model) {
 
-        // TODO: Implementar rmi para buscar o txt
-        response.addCookie(new Cookie("username", username));
-        response.addCookie(new Cookie("password", password));
+        try {
+            // Invoke the search method on the remote Gateway service
+            MetodosRMIGateway metodosGateway = null;
+            metodosGateway = (MetodosRMIGateway) Naming.lookup("rmi://" + ConnectionsEnum.GATEWAY.getIP() + ":" + ConnectionsEnum.GATEWAY.getPort() + "/gateway");
 
-        return "redirect:/";
+            int resultado = metodosGateway.autenticarCliente(username, password);
+
+            if (resultado != -1) {
+                response.addCookie(new Cookie("username", resultado+""));
+                response.addCookie(new Cookie("username", username));
+                response.addCookie(new Cookie("password", password));
+                return "redirect:/";
+            } else {
+                model.addAttribute("resultadoOperacao", "Invalid username or password.");
+                return "login";
+            }
+
+        } catch (Exception e) {
+            // Handle any exceptions
+            e.printStackTrace();
+            model.addAttribute("errorMessage", "An error occurred during search.");
+        }
+
+        return "login";
     }
 
     //account types: 0 para normal, 1 para admin
