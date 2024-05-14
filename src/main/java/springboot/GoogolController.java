1c7f00ae029394fcf686b67b809d9bc24315fb3a
José Rodrigues <joser0drigueswork@gmail.com>
2024-05-14 22:38:29 +0100

    chat gpt que nao funciona e com o token ali mesmo rip

--- src/main/java/springboot/GoogolController.java
+++ src/main/java/springboot/GoogolController.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GoogolController {
 
-	@GetMapping("/")
-	public String index(@CookieValue(value = "username", defaultValue = "-") String username,
-						@CookieValue(value = "accountType", defaultValue = "0") String accountType,
-						Model model) {
+    @GetMapping("/")
+    public String index(@CookieValue(value = "username", defaultValue = "-") String username,
+                        @CookieValue(value = "accountType", defaultValue = "0") String accountType,
+                        Model model) {
 
-		model.addAttribute("isLoggedIn", !username.equals("-"));
-		model.addAttribute("username", username);
+        model.addAttribute("isLoggedIn", !username.equals("-"));
+        model.addAttribute("username", username);
 
-		return "homepage";
-	}
+        return "index";
+    }
 }
